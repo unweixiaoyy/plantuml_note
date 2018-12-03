d0bae9e9b951c011ff1026d67aaa35b8322c3e26
jialong <weixiao870428@163.com>
2018-12-03 18:34:56 +0800

    初次提交，放到一个空项目里面。 拉到AndroidStudio或其他IDE会比单纯的存储文件更好，毕竟在项目里面查看源码方便！

---
+++ app/app/src/main/java/note/plantuml/jialong/com/plantuml_note/MainActivity.java
@@ -0,0 +1,12 @@
+package note.plantuml.jialong.com.plantuml_note;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
